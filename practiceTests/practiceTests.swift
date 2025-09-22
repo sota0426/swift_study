f7af40299a147277b03ac8c0616a8bb1dc05f395
sota <sota0426@gmail.com>
2025-09-22 22:26:53 +0900

    Initial Commit

---
+++ practiceTests/practiceTests.swift
@@ -0,0 +1,17 @@
+//
+//  practiceTests.swift
+//  practiceTests
+//
+//  Created by 飯森壮太 on 2025/09/22.
+//
+
+import Testing
+@testable import practice
+
+struct practiceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
